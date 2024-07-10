08cc35e93476059baf9783ada73041b2154390f4
Gleb Yasynskyi <gleb.yasinsky@gmail.com>
2024-07-10 23:27:45 +0300

    hw8

--- js/task-1.js
+++
@@ -1,9 +0,0 @@
-const cat = document.querySelectorAll('#categories .item');
-console.log(`Number of categories: ${cat.length}`);
-
-cat.forEach(item => {
-  const title = item.querySelector('h2').textContent;
-  const size = item.querySelectorAll('li').length;
-  console.log(`Category: ${title}`);
-  console.log(`Elements: ${size}`);
-});
